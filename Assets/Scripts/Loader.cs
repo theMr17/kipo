fb669b0fcae7691887fdc7048d2df7de1bf24b63
Saptak Manna <saptakmanna100@gmail.com>
2025-07-30 19:19:03 +0530

    Add main menu scene and loading functionality
    - Created MainMenuScene with associated meta file.
    - Implemented Loader class for scene management, including loading and network loading methods.
    - Added LoaderCallback class to handle scene loading callbacks.
    - Developed MainMenuUI class to manage UI interactions, including play and quit buttons.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,27 @@
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+
+public static class Loader {
+    public enum Scene
+    {
+        LoadingScene,
+        MainMenuScene,
+        LobbyScene,
+    }
+
+    public static Scene targetScene;
+
+    public static void Load(Scene targetScene) {
+        Loader.targetScene = targetScene;
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoadNetwork(Scene targetScene) {
+        NetworkManager.Singleton.SceneManager.LoadScene(targetScene.ToString(), LoadSceneMode.Single);
+    }
+
+    public static void LoaderCallback() {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUi.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour {
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake() {
+        playButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.LobbyScene);
+        });
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
+}
