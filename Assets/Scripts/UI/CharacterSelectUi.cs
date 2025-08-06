943553b221af384978a08684ff5656908b596cb2
Saptak Manna <saptakmanna100@gmail.com>
2025-08-06 15:44:00 +0530

    Add copy code button functionality to CharacterSelectUi

--- Assets/Scripts/UI/CharacterSelectUi.cs
+++ Assets/Scripts/UI/CharacterSelectUi.cs
@@ -6,10 +6,18 @@ using Unity.Services.Lobbies.Models;
 public class CharacterSelectUi : MonoBehaviour {
     [SerializeField] private Button readyButton;
     [SerializeField] private TextMeshProUGUI lobbyCodeText;
+    [SerializeField] private Button copyCodeButton;
 
-    private void Awake() {
-        readyButton.onClick.AddListener(() => {
+    private void Awake()
+    {
+        readyButton.onClick.AddListener(() =>
+        {
             CharacterSelectReady.Instance.SetPlayerReady();
+        });
+        copyCodeButton.onClick.AddListener(() =>
+        {
+            Lobby lobby = LobbyManager.Instance.GetLobby();
+            GUIUtility.systemCopyBuffer = lobby.LobbyCode;
         });
     }
 
